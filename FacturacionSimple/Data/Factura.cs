f507395d0fd788b8c4f39a03f6cda0cff69d9e49
Felix Benzan <139989847+fbenzan@users.noreply.github.com>
2024-02-14 20:18:02 -0400

    Creando las clases para la data para las facturas y los detalles.

---
+++ FacturacionSimple/Data/Factura.cs
@@ -0,0 +1,11 @@
+﻿namespace FacturacionSimple.Data;
+
+public class Factura
+{
+    public int Numero { get; set; }
+    public string Cliente { get; set; } = "";
+    public DateTime Fecha { get; set;}
+    public List<FacturaDetalle> Detalles { get; set; }
+    = new ();
+    public decimal Total => (Detalles!=null&&Detalles.Any()) ? Detalles.Sum(d=>d.SubTotal) : 0;
+}
---
+++ FacturacionSimple/Data/FacturaDetalle.cs
@@ -0,0 +1,9 @@
+﻿namespace FacturacionSimple.Data;
+
+public class FacturaDetalle
+{
+    public string Producto { get; set; } = "";
+    public int Cantidad { get; set; }
+    public decimal Precio { get; set; }
+    public decimal SubTotal => Cantidad * Precio;
+}
